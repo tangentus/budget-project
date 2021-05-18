79cc55bc2186319439be2abea95b128f86c8cd3b
Trevor Broaddus <trevor.broaddus@daveramsey.com>
2021-05-18 05:38:54 -0500

    Add allowances resources/controller

---
+++ app/controllers/allowances_controller.rb
@@ -0,0 +1,22 @@
+class AllowancesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
