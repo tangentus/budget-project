c1f31960ad36d484087f101611d4dd9fb1ac08b8
Trevor Broaddus <trevor.broaddus@daveramsey.com>
2021-05-18 05:53:38 -0500

    Write all controller/crud logic for allowances

--- app/controllers/allowances_controller.rb
+++ app/controllers/allowances_controller.rb
@@ -1,22 +1,68 @@
 class AllowancesController < ApplicationController
+  attr_reader :allowance, :allowances
+  helper_method :allowance, :allowances
+
+  before_action :set_allowance, only: %i[ show edit update destroy ]
+
+  # GET /allowances or /allowances.json
   def index
+    @allowances = Allowance.all
   end
 
+  # GET /allowances/1 or /allowances/1.json
   def show
   end
 
-  def edit
-  end
-
-  def update
+  # GET /allowances/new
+  def new
+    @allowance = Allowance.new
+    @splits = [allowance.splits.new]
   end
 
   def create
+    @allowance = Allowance.new(allowance_params)
+
+    if allowance.save
+      respond_to do |format|
+        format.html { redirect_to allowance, notice: "Allowance was successfully created." }
+        format.json { render :show, status: :created, location: allowance }
+      end
+    end
   end
 
-  def new
+  # GET /allowances/1/edit
+  def edit
   end
 
+  # PATCH/PUT /allowances/1 or /allowances/1.json
+  def update
+    respond_to do |format|
+      if allowance.update(allowance_params)
+        format.html { redirect_to allowance, notice: "Allowance was successfully updated." }
+        format.json { render :show, status: :ok, location: allowance }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: allowance.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /allowances/1 or /allowances/1.json
   def destroy
+    allowance.destroy
+    respond_to do |format|
+      format.html { redirect_to allowances_url, notice: "Allowance was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_allowance
+    @allowance = Allowance.find(params[:id])
+  end
+
+  def allowance_params
+    params.require(:allowance).permit(:name, :starting_amount, :planned, :category, :start_date, :end_date)
   end
 end
